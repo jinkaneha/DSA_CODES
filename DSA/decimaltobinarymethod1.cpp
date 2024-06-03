6145b0d079c672709621a1df692c055f5233b880
jinkaneha <nehasrinivas1107@gmail.com>
2024-06-03 21:34:45 +0530

    binarytodecimalcode

---
+++ DSA/decimaltobinarymethod1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int decimaltobinarymethod1(int n){
+    int binaryno=0;
+    int i=0;
+    while(n>0){
+        int bit=n%2;
+        binaryno=bit*pow(10,i++)+binaryno;
+        n=n/2;
+    }
+    return binaryno;
+}
+int main(){
+    int n;
+    cin >> n;
+    int binary=decimaltobinarymethod1(n);
+    cout << binary << endl;
+    // cout <<binary;
+}
