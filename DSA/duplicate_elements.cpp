244c50cc738551949bee2ab30386c879dad16e7f
jinkaneha <nehasrinivas1107@gmail.com>
2024-06-05 23:42:05 +0530

    duplicateele_optimal

---
+++ DSA/duplicate_elements.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int i=0;
+        while(nums[i]!=nums[nums[i]]){
+            swap(nums[i],nums[nums[i]]);
+          //  i++;
+        }
+        return nums[i];
+
+        
+    }
+};
