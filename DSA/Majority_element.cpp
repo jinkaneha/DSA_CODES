965fc43dccb5c257dae622f5e56062ddfd3f2261
jinkaneha <nehasrinivas1107@gmail.com>
2024-06-05 23:43:51 +0530

    majority_element

---
+++ DSA/Majority_element.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        unordered_map<int,int> mp;
+        for(auto it:nums){
+            mp[it]++;
+            if(mp[it]>nums.size()/2){
+                return it;
+            }
+        }
+        return -1;
+    }
+};
