94ec724638b93eb4021ac3183fc208772d0b2f02
jinkaneha <nehasrinivas1107@gmail.com>
2024-06-05 23:39:31 +0530

    sortcolors_0s,1s,2s

---
+++ DSA/sort_colors.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int low=0;
+        int mid=0;
+        int end=nums.size()-1;
+        while(mid<=end){
+        if(nums[mid]==0){
+            swap(nums[low],nums[mid]);
+            low++;
+            mid++;
+        }
+        else if(nums[mid]==1){
+            mid++;
+        }
+        else{
+            swap(nums[mid],nums[end]);
+            end--;
+        }
+        }
+
+        return;
+    }
+};
