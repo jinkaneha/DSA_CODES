1e3bef67da3d00fe6abe31e8406be52c787bc86c
jinkaneha <nehasrinivas1107@gmail.com>
2024-06-07 19:32:20 +0530

    insertAtTail

---
+++ DSA/ll(insertAttail).cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+
+class Node{
+public:
+    int data;
+    Node* next;
+
+    Node(){
+        this->data = 0;
+        this->next = NULL;
+    }
+    Node(int data){
+        this->data = data;
+        this->next = NULL;
+    }
+};
+
+void insertAtTail(Node* &head, Node* &tail, int data) {
+    Node* newNode = new Node(data);
+    if (tail == NULL) {
+        head = tail = newNode;
+    } else {
+        tail->next = newNode;
+        tail = newNode;
+    }
+}
+
+void print(Node* head) {
+    Node* temp = head;
+    while (temp != NULL) {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+int main() {
+    Node* head = NULL;
+    Node* tail = NULL;
+
+    insertAtTail(head, tail, 10);
+    insertAtTail(head, tail, 20);
+    insertAtTail(head, tail, 16);
+
+    print(head);
+
+    return 0;
+}
