d8d33b2a9f63988ccdd90c377c0a9b1c1ac1fb22
jinkaneha <nehasrinivas1107@gmail.com>
2024-06-08 22:07:21 +0530

    cleardigits

---
+++ DSA/cleardigits.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string clearDigits(string s) {
+        string res="";
+        for(char c:s){
+            if(!isdigit(c)){
+                res.push_back(c);
+            }
+            else{
+                res.pop_back();
+            }
+        }
+        return res;
+
+        }
+    
+};
