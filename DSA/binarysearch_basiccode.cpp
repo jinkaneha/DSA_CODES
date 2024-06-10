d1eff2e5c63124b14a03449646f8bc4d41c5b00c
jinkaneha <nehasrinivas1107@gmail.com>
2024-06-10 20:55:05 +0530

    binarysearch

---
+++ DSA/binarysearch_basiccode.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int low=0;
+        int high=nums.size()-1;
+        int mid=low+(high-low)/2;
+        while(low<=high){
+        if(nums[mid]==target) return mid;
+        if(target>nums[mid]){
+            low=mid+1;
+        }
+        if(target<nums[mid]){
+            high=mid-1;
+        }
+           mid=low+(high-low)/2;
+        }
+        return -1;
+    }
+};
