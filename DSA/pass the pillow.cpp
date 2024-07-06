d2676951b161810a9335697108ed544c8d464541
jinkaneha <nehasrinivas1107@gmail.com>
2024-07-06 23:44:21 +0530

    pass the pilow

---
+++ DSA/pass the pillow.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int passThePillow(int n, int time) {
+        
+        int ans=1;
+        int rev=false;
+        while(time--)
+        {
+            if(!rev)
+            {
+                
+                ans++;
+                cout<<"time "<<time<<" " <<ans<<" "<<endl;
+                
+
+            }
+            else
+            {
+                
+                ans--;
+                  cout<<"time "<<time<<" " <<ans<<" "<<endl;
+                
+            }
+            
+            if(ans%n==0 || ans==1) rev=!rev;
+           
+        }
+        return ans;
+        
+    }
+};
