be69d25e62719d6ac108f9b3d188697ed022865f
jinkaneha <nehasrinivas1107@gmail.com>
2024-07-08 23:06:11 +0530

    doubly linked list

--- DSA/Linked listt/Doublylinkedlist-insertion.cpp
+++ DSA/Linked listt/Doublylinkedlist-insertion.cpp
@@ -57,6 +57,39 @@ void insertAtTail(Node* &head,Node* &tail,int data){
   newNode->prev=tail;
   tail=newNode;
 }
+void insertAtPosition(Node* &head,Node* &tail,int pos,int data){
+  Node* newNode=new Node(data);
+  if(head==NULL){
+    head = newNode;
+    tail = newNode;
+    return;
+  }
+  int len=getlength(head);
+  if(pos==1){
+    insertAtHead(head,tail,data);
+    return;
+  }
+  if(pos>len){
+    insertAtTail(head,tail,data);
+    return;
+  }
+  //if not first and last
+  //step2:find prev and curr
+  int i=1;
+   Node* prevnode=head;
+  while(i<pos-1){
+   prevnode=prevnode->next;
+   i++;
+  }
+  Node* curr=prevnode->next;
+  //step3:
+  prevnode->next=newNode;
+  newNode->prev=prevnode;
+  curr->prev=newNode->next;
+  newNode->next=curr;
+
+
+}
 int main(){
   Node* head=NULL;
   Node* tail=NULL;
@@ -67,6 +100,13 @@ int main(){
   cout << endl;
   insertAtTail(head,tail,400);
   insertAtTail(head,tail,500);
+  print(head);
+  cout << endl;
+  insertAtPosition(head,tail,1,99);
+  insertAtPosition(head,tail,2,101);
+  insertAtPosition(head,tail,7,401);
+   insertAtPosition(head,tail,8,402);
+  insertAtPosition(head,tail,10,600);
   print(head);
   return 0;
 } 
