d52a38877c98daa77da4f25536886f54b46a0230
jinkaneha <nehasrinivas1107@gmail.com>
2024-07-09 12:56:27 +0530

    isprime

---
+++ DSA/Basic math/isprime.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int isPrime(int N) {
+        
+        if (N <= 1) return false; 
+        if (N <= 3) return true; 
+
+        if (N % 2 == 0 || N % 3 == 0) return false;
+
+        
+        for (int i = 5; i * i <= N; i += 6) {
+            if (N % i == 0 || N % (i + 2) == 0) return false;
+        }
+
+        return true; 
+    }
+};
+
+
